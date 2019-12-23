e386966bde73c12d91fb205a28484e59a520e864
anjramas <59171413+anjramas@users.noreply.github.com>
2019-12-23 21:18:56 +0530

    Update dnac_config.py

--- dnac_config.py
+++ dnac_config.py
@@ -1,5 +1,5 @@
 import os
-DNAC=os.environ.get('DNAC','10.104.240.91')
+DNAC=os.environ.get('DNAC','<DNAC_IP_Address>')
 DNAC_PORT=os.environ.get('DNAC_PORT',443)
-DNAC_USER=os.environ.get('DNAC_USER','admin')
-DNAC_PASSWORD=os.environ.get('DNAC_PASSWORD','Maglev123')
+DNAC_USER=os.environ.get('DNAC_USER','<DNAC_UI_Username>')
+DNAC_PASSWORD=os.environ.get('DNAC_PASSWORD','<DNAC_UI_Password>')
