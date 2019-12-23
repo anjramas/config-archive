5e1af440775056e41c04f80b5a18392631c4fe8e
anjramas <59171413+anjramas@users.noreply.github.com>
2019-12-23 19:31:16 +0530

    Add files via upload

---
+++ dnac_config.py
@@ -0,0 +1,5 @@
+import os
+DNAC=os.environ.get('DNAC','10.104.240.91')
+DNAC_PORT=os.environ.get('DNAC_PORT',443)
+DNAC_USER=os.environ.get('DNAC_USER','admin')
+DNAC_PASSWORD=os.environ.get('DNAC_PASSWORD','Maglev123')
